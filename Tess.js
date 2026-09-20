9f77e4ec51b663e168c346c8b6b330d5426da104
YuziAnakyangSangatBaik <azkad2840@gmail.com>
2026-09-20 20:22:12 +0700

    Add initial Tess.js configuration object

---
+++ Tess.js
@@ -0,0 +1,5 @@
+{
+"url_aktif": "http://luwwyxyzstoreprivate.pteroq.xyz:2382", "updated_at": "2026-06-21T16:21:59.711Z",
+"updated_by": "Yuzi",
+"url_rat": "ws://luwwyxyzstoreprivate.pteroq.xyz:2382"
+}
